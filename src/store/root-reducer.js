5e9640d4b3801c4353f69ebeac902e9e5e4964a3
Alex <specmashalexeysh@gmail.com>
2022-03-29 23:13:43 +0300

    commit

---
+++ src/store/root-reducer.js
@@ -0,0 +1,8 @@
+import {combineReducers} from "redux";
+import {positionReducer} from "./positions/position-reduser";
+import { filterReducer } from "./filters/filter-reducer";
+
+export const rootReducer = combineReducers({
+	position: positionReducer,
+	filters: filterReducer,
+});
